699bd1c9b9e4f74d7b5bea9a684f628c1e8c267a
LaislaSte <stephenielaisla@gmail.com>
2024-04-10 21:17:49 -0300

    prova v1

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.render("cadastrar")
+    res.render("cadastrar-page")
 });
 
 app.post("/cadastrar", (req, res) => {
